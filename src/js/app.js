454a01ea1ba44a4aa6110c4a85c9a4906bd98cfa
Alexander Kiselev <alexander.kiselev@mail.ru>
2018-10-18 10:47:37 +0300

    Wip top menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,37 @@
-console.log('Project started');
+import $ from '../../node_modules/jquery';
+
+$(() => {
+    const $MENU = $('.main-menu');
+    const $HAMBURGER = $('.hamburger');
+    const $MOUSEDOWN = $('.mouse-down');
+    const $SCROLLTOTOP = $('.scroll-to-top');
+
+    $HAMBURGER.click((e) => {
+        e.preventDefault();
+        $MENU.toggleClass('-active');
+        $HAMBURGER.toggleClass('is-active')
+    });
+
+    $MOUSEDOWN.click((e) => {
+        e.preventDefault();
+        const WH = window.innerHeight;
+
+        $('html').animate({
+            scrollTop: `${WH - 56}px`
+        }, 400);
+    });
+
+    $SCROLLTOTOP.click((e) => {
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: 0
+        }, 400);
+    });
+
+    $MENU.find('.scroll-to-top, a').click((e) => {
+        e.preventDefault();
+        $MENU.toggleClass('-active');
+        $HAMBURGER.toggleClass('is-active');
+
+    })
+});
