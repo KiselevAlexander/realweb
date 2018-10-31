6fef5bce808582aedd26dd6a559c9163a4be1871
Alexander Kiselev <kiselev@raketa.im>
2018-10-31 16:16:26 +0300

    Changed receive email

--- src/php/forms.php
+++ src/php/forms.php
@@ -23,7 +23,7 @@ $FIELDS = [
     'message' => 'Сообщение'
 ];
 
-$to = 'lv@realweb.ru, keesik@yandex.ru';
+$to = 'xxyearsrealweb@gmail.com, dyx624@gmail.com';
 
 $subject = (array_key_exists('type', $data) && array_key_exists($data['type'], $FORMS))
     ? $FORMS[$data['type']]
